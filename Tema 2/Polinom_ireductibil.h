caf1cc51d7b130e46dddd50c0f4520d27d6a2c5c
Vartic Mihai-Vlad <61534490+Varothex@users.noreply.github.com>
2020-05-05 02:23:17 +0300

    Create Polinom_ireductibil.h

---
+++ Tema 2/Polinom_ireductibil.h
@@ -0,0 +1,12 @@
+/*#ifndef POLINOM_IREDUCTIBIL_H
+#define POLINOM_IREDUCTIBIL_H
+#include "Polinom.h"
+
+class Polinom_ireductibil: public Polinom
+{
+    public:
+        Polinom_ireductibil();
+        virtual ~Polinom_ireductibil();
+};
+
+#endif*/

b3d97ed4f9de397235ac6fcaca4f72789ed9c23b
Vartic Mihai-Vlad <61534490+Varothex@users.noreply.github.com>
2020-05-11 02:11:56 +0300

    Update Monom.cpp

--- Tema 2/Monom.cpp
+++ Tema 2/Monom.cpp
@@ -1,19 +1,58 @@
 #include <iostream>
 #include "Monom.h"
+#include "Polinom.h"
+#include "Polinom_ireductibil.h"
 
 using namespace std;
 
-Monom::Monom(int g, float c)
+/**Cerinta 6: Polinoame reprezentate ca tablouri unidimensionale (prin gradul polinomului si
+vectorul coeficientilor)
+
+Se dau urmatoarele clase:
+- Clasa Monom(int grad, float coef);
+- Clasa Polinom(int nr_monoame, Monom *m);
+- Polinom_ireductibil: Polinom.
+
+Dacă vi se pare mai natural, puteți sa aveți clasa Polinom ca clasa de baza pentru
+Polinom_Ireductibil și pentru Polinom_reductibil:
+- Clasa Polinom(int nr_monoame, Monom *m) – clasa abstracta;
+- Polinom_ireductibil: Polinom;
+- Polinom_reductibil: Polinom.
+
+Afisarea unui polinom reductibil să fie făcută ca produs de 2 polinoame.
+
+Clasa derivata trebuie sa contina o metoda care sa aplice criteriul lui Eisenstein de verificare
+a ireductibilitatii polinoamelor.*/
+
+int main()
 {
-    grad = g;
-    coef = c;
+    Monom x(3, 4.0), y(4, 5.0), z(5, 6.0);
+    x.print();
+    y.print();
+    z.print();
+
+    Polinom xyz(5);
+    xyz.add(&x);
+    xyz.add(&y);
+    xyz.add(&z);
+    xyz.print();
+
+    xyz.ireductibil();
+
+    cout << "\n";
+
+    Monom a(1, 3), b(5, 34), c(10, 5);
+    a.print();
+    b.print();
+    c.print();
+
+    Polinom abc(10);
+    abc.add(&a);
+    abc.add(&b);
+    abc.add(&c);
+    abc.print();
+
+    abc.ireductibil();
+
+    return 0;
 }
-
-void Monom::print()
-{
-    cout << coef << "x^" << grad << "\n";
-}
-
-Monom::Monom(const Monom &mon) = default;
-
-Monom::~Monom() = default;

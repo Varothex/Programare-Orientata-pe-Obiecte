d01808140799c75e930302e44171a4b88620b14d
Vartic Mihai-Vlad <61534490+Varothex@users.noreply.github.com>
2020-05-12 00:44:46 +0300

    Update main.cpp

--- Tema 2/main.cpp
+++ Tema 2/main.cpp
@@ -1,30 +1,19 @@
 #include <iostream>
 #include "Monom.h"
 #include "Polinom.h"
+#include "Polinom_ireductibil.h"
 
 using namespace std;
 
-/**Cerinta 6: Polinoame reprezentate ca tablouri unidimensionale (prin gradul polinomului si
-vectorul coeficientilor)
-
-Se dau urmatoarele clase:
-- Clasa Monom(int grad, float coef);
-- Clasa Polinom(int nr_monoame, Monom *m);
-- Polinom_ireductibil: Polinom.
-
-Dacă vi se pare mai natural, puteți sa aveți clasa Polinom ca clasa de baza pentru
-Polinom_Ireductibil și pentru Polinom_reductibil:
-- Clasa Polinom(int nr_monoame, Monom *m) – clasa abstracta;
-- Polinom_ireductibil: Polinom;
-- Polinom_reductibil: Polinom.
-
-Afisarea unui polinom reductibil să fie făcută ca produs de 2 polinoame.
-
-Clasa derivata trebuie sa contina o metoda care sa aplice criteriul lui Eisenstein de verificare
-a ireductibilitatii polinoamelor.*/
-
 int main()
 {
+    /*Monom x, y, z;
+    cin >> x >> y >> z;
+    cout << x << y << z;*/
+
+    /*Monom copiex = x;
+    cout << copiex;*/
+
     Monom x(3, 4.0), y(4, 5.0), z(5, 6.0);
     x.print();
     y.print();
@@ -36,29 +25,36 @@ int main()
     xyz.add(&z);
     xyz.print();
 
-    if(xyz.ireductibil())
-        cout << "Este ireductibil.";
-
-    cout << "\n" << "\n";
-
-    Monom a(1, 3.2), b(4, 34.1), c(10, 1);
-    a.print();
-    b.print();
-    c.print();
-
-    Polinom abc(10);
-    abc.add(&a);
-    abc.add(&b);
-    abc.add(&c);
-
+    xyz.ireductibil();
 
     cout << "\n";
 
-    if(abc.ireductibil())
-        cout << "Este ireductibil.";
 
-    abc.print();
-    abc.print();
+    /*Monom a, b, c;
+    cin >> a >> b >> c;
+    cout << a << b << c;*/
+
+    /*Monom a(1, 3), b(5, 34), c(10, 5);
+    a.print();
+    b.print();
+    c.print();*/
+
+    Polinom abc;
+    cin >> abc;
+    cout << abc;
+
+    /*Polinom abc(10);
+    abc.add(&a);
+    abc.add(&b);
+    abc.add(&c);
+    abc.print();*/
+
+    //abc.ireductibil();
+
+    /*Polinom_ireductibil qwerty;
+    cout << qwerty.nr_monoame;
+
+    qwerty.print();*/
 
     return 0;
 }

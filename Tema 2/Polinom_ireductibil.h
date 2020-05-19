f5a4802246ebd6ca5906a62a484a68bc10403734
Vartic Mihai-Vlad <61534490+Varothex@users.noreply.github.com>
2020-05-20 01:24:07 +0300

    Update Polinom_ireductibil.h

--- Tema 2/Polinom_ireductibil.h
+++ Tema 2/Polinom_ireductibil.h
@@ -1,12 +1,22 @@
-/*#ifndef POLINOM_IREDUCTIBIL_H
+#ifndef POLINOM_IREDUCTIBIL_H
 #define POLINOM_IREDUCTIBIL_H
 #include "Polinom.h"
 
 class Polinom_ireductibil: public Polinom
 {
-    public:
-        Polinom_ireductibil();
-        virtual ~Polinom_ireductibil();
+public:
+
+    friend istream &operator >>(istream& input, Polinom &p1);
+
+    friend ostream &operator <<(ostream& output, Polinom p1);
+
+    Polinom_ireductibil(int n=1);
+
+    Polinom_ireductibil(const Polinom &p);
+
+    ~Polinom_ireductibil();
+
+    bool ireductibil();
 };
 
-#endif*/
+#endif
